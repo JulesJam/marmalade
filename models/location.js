5424ffa11e378db167d94f50ecd0e109ff9d7fa4
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2017-12-07 18:53:42 +0100

    model updated with additional fields

--- models/location.js
+++ models/location.js
@@ -2,8 +2,12 @@ var mongoose = require('mongoose');
 
 var locationSchema = mongoose.Schema({
   locationName: { type: String, required: true},
+  locationTown: { type: String, required: true},
+  locationPostcode: { type: String},
+  locationCountry: { type: String, default: "UK"},
   description: { type: String},
-  dateAdded: { type: Date},
+  dateAdded: { type: Date, default: Date.now},
+  locationMainImage: {type: String},
   tag: { type: String},
   lat: { type: String},
   long:{ type: String},
